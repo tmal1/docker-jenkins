184563b657ee7dddd1693cf96aeecbbbd5dd2931
tmal1 <malina19pl@gmail.com>
2017-05-22 15:16:32 +0200

    Update fakeuname.sh

--- slave/rpmbuilder/fakeuname.sh
+++ slave/rpmbuilder/fakeuname.sh
@@ -6,9 +6,9 @@ kernel=$($uname -s)
 nodename=$($uname -n)
 kernel_rel=$KERNEL_VERSION
 kernel_ver=$($uname -v)
-machine=$MACHINE
-processor=$PROCESSOR
-hw=$HW
+machine=$($uname -m)
+processor=$($uname -p)
+hw=$($uname -i)
 os=$($uname -o)
 
 uname_output() {
